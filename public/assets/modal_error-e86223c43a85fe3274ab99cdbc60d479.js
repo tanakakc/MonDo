de35b970a8bc484a8f86c7a77c54d8f33184b15e
Keishi Tanaka <kc@toiee.jp>
2013-11-08 19:36:55 +0900

    assets precompiled

--- public/assets/modal_error-e86223c43a85fe3274ab99cdbc60d479.js
+++
@@ -1,1 +0,0 @@
-$(function(){$("#mondo_answer").click(function(){var n=0;return $(this).closest("form").find("textarea").each(function(){""==$(this).val()&&(n+=1)}),4==n?($("#mondo_edit_modal .modal-body").prepend('<div class="alert alert-danger">ERROR:1つ以上入力してください</div>'),!1):void 0})});
