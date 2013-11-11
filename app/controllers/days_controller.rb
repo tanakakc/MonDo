15f7b5de5dbddd5297cd27449cdb25a28fe27f0d
Keishi Tanaka <kc@toiee.jp>
2013-11-11 12:37:38 +0900

    FIXED timezone for passed_days

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -120,7 +120,7 @@ before_action :How_many_days_passed?, only: [:index, :show, :edit]
   
     # スタートして今、何日目かを確認
     def How_many_days_passed?
-      @time_now = Time.now #今の時刻
+      @time_now = Time.zone.now #今の時刻
       @first_day = Day.find_by(user_id: current_user.id, date: 1) #レコードを1つ取得
       @first_day = @first_day.created_at - 3.hours #1日目レコードの作成日時を取得
       @first_day = @first_day.change(hour: 3, minutes: 0, seconds: 0)
