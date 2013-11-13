40650c2cc350b7f395e32ba4275c8557c23b69a3
Keishi Tanaka <kc@toiee.jp>
2013-11-13 18:25:53 +0900

    assets precompile #7

--- public/assets/modal_error-1c8e3ebc177018499e846742cae890ed.js
+++ public/assets/modal_error-1c8e3ebc177018499e846742cae890ed.js
@@ -5,6 +5,7 @@ $(function(){
 		   if($(this).val()==""){empty = empty + 1} 
 	    });
 	    if(empty == 4){
+	    $("#mondo_edit_modal .alert").remove();
 	    $("#mondo_edit_modal .modal-body").prepend('<div class="alert alert-danger">ERROR:1つ以上入力してください</div>');
 	    return false;
 	    }
